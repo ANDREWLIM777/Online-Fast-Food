c52f13653975f683508959fa75bbb5e9c1721571
NGJINYANG <jinyang8848@gmail.com>
2025-06-10 10:46:32 +0800

    halomotor

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '22503f4037e6936f49c72cef31ff775971e3db5b',
+        'reference' => '940ef34924d528b5641e14bd935f5cb1a667810d',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '22503f4037e6936f49c72cef31ff775971e3db5b',
+            'reference' => '940ef34924d528b5641e14bd935f5cb1a667810d',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
